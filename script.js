4a20bace8247af1109885ad23cf50d1775b352fc
Aymen Brahimi <brahim_a@etna-alternance.net>
2026-01-28 21:24:53 +0100

    Add ebook landing page

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Remplace ces liens par tes liens réels
+// document.getElementById("gumroadLink").href = "https://ton-lien-gumroad";
+// document.getElementById("payhipLink").href = "https://ton-lien-payhip";
